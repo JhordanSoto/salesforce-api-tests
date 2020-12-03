9687b62e4c7bf41b118e165111e273a90d42005f
julia-jpg <61889326+julia-jpg@users.noreply.github.com>
2020-12-02 20:39:25 -0400

    [develop] Base Project (#2)
    * add initial files
    
    * solve observacion PR
    
    * add dependencies jitpack.io
    
    Co-authored-by: juls-es <escalantejulia80@gmail.com>

---
+++ src/test/java/org/fundacionjala/salesforce/runner/Runner.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.salesforce.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+
+/**
+ * Cucumber TestNG runner class.
+ */
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = {"src/test/resources/features"},
+        glue = {"org.fundacionjala.salesforce"}
+)
+public final class Runner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+    /**
+     * Executes code before all scenarios.
+     */
+    @BeforeTest
+    public void beforeAllScenarios() {
+        System.setProperty("dataproviderthreadcount", "1");
+    }
+}
